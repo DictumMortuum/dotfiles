3a67881c5830d02c2dd7bdb711e7697dd299aec2
Dimitris Raviolos <dictummortuum339@gmail.com>
2016-07-17 12:37:53 +0300

    [zsh] Fixed symlink path

--- .zsh/install.sh
+++ .zsh/install.sh
@@ -1,10 +1,10 @@
 main() {
 	cd $HOME/.oh-my-zsh/custom
-	ln -s $HOME/zsh/openbet.zsh
+	ln -s $HOME/.zsh/openbet.zsh
 	cd themes
-	ln -s $HOME/zsh/anmol.zsh-theme
+	ln -s $HOME/.zsh/anmol.zsh-theme
 	cd ../plugins
-	ln -s $HOME/zsh/zsh-syntax-highlighting
+	ln -s $HOME/.zsh/zsh-syntax-highlighting
 	cd
 }
 
