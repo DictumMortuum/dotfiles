3aa0d9cf58dcac4b24ea4460371b3474bb976a4b
DictumMortuum <theio.vrefos@gmail.com>
2016-08-02 21:27:56 +0300

    [tclsh] do not overwrite TCLLIBPATH

--- .zsh/autoload/tclsh.zsh
+++ .zsh/autoload/tclsh.zsh
@@ -7,4 +7,4 @@ PATH="$HOME/.tclsh/bin:$PATH"
 # This path will be added to auto_path tcl global.
 # It will check that folder and its subfolders for pkgIndex.tcl files to source.
 # If a package is required.
-export TCLLIBPATH="$HOME/.tclsh"
+export TCLLIBPATH="$HOME/.tclsh:$TCLLIBPATH"
