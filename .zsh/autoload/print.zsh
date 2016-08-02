fb93112a411ccf080b6aa309c1ab96bef01fa2b9
DictumMortuum <theio.vrefos@gmail.com>
2016-08-03 01:40:25 +0300

    format

--- .zsh/autoload/print.zsh
+++ .zsh/autoload/print.zsh
@@ -1,6 +1,6 @@
 # rfc2396 url encoding (by Bart Schaefer)
 function @urlencode() {
-    setopt localoptions extendedglob
-    input=( ${(s::)1} )
-    print ${(j::)input/(#b)([^A-Za-z0-9_.--version\'\(\)-])/%$(([##16]#match))}
+  setopt localoptions extendedglob
+  input=( ${(s::)1} )
+  print ${(j::)input/(#b)([^A-Za-z0-9_.--version\'\(\)-])/%$(([##16]#match))}
 }
