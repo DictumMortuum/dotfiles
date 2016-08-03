b29f9498a76dbb959626d1aed04220b4974912b4
DictumMortuum <theio.vrefos@gmail.com>
2016-08-03 16:30:45 +0300

    [zsh] ignoring ls and history commands from history

--- .zsh/autoload/alias.zsh
+++ .zsh/autoload/alias.zsh
@@ -1,2 +1,8 @@
 alias dictum="~/Code/dictum/tools/client.js"
 alias apt-update="sudo apt-get update ; sudo apt-get upgrade ; sudo apt-get autoclean ; sudo apt-get autoremove"
+
+# Assumes HIST_IGNORE_SPACE is set
+# making aliases to certain commands to start with space to be ignored from history
+alias ls=" ls --color=tty"
+alias ll=" ls -lh"
+alias history=" fc -il 1"
