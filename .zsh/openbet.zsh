0c2a7acdbc10ffc30dda8a4f9f4940eacd5b8533
DictumMortuum <theio.vrefos@gmail.com>
2016-08-03 13:09:35 +0300

    [zsh] updating path for work

--- .zsh/openbet.zsh
+++ .zsh/openbet.zsh
@@ -14,7 +14,6 @@ ulimit -v 4194304
 
 # TCL
 export TCL=/opt/tcl8.5
-export TCL=/opt/tcl8.5
 export TCLEXTENSION=tcl
 export TCLLIBPATH='/usr/lib/tcl8.5:/space/pmu/tcl8.5.7/lib/64:/space/pmu/tcl8.5.7/lib/ot_tibems0.2:/opt/tcl/lib:/opt/informix/lib:/opt/informix/lib/esql'
 export TCLPRO=/opt/TclPro/linux-ix86/
@@ -22,7 +21,16 @@ export TCL_PRO=/opt/TclPro/linux-ix86/
 export LD_LIBRARY_PATH='/usr/lib:/space/pmu/tibems/lib/64:/space/pmu/tibems/lib:/space/pmu/tcl8.5.7/lib:/opt/informix/lib:/opt/informix/lib/esql:/opt/informix/lib:/lib'
 
 # Path
-PATH="$PATH:/opt/TclDevKit/bin:/opt/apache-ant/bin:/opt/apache-maven/bin:/opt/informix/bin:/opt/puppetlabs/bin:/shared/orbis/Support/CustomerDB:/shared/prj/openbet/appserv/rhel/6/x86_64/bin:/shared/prj/openbet/liveserv/rhel/6/x86_64/sbin:/usr/java/latest/bin:/usr/lib64/qt-3.3/bin"
+test -d "/bin" && PATH="/bin:$PATH"
+test -d "/opt/apache-ant/bin" && PATH="/opt/apache-ant/bin:$PATH"
+test -d "/opt/apache-maven/bin" && PATH="/opt/apache-maven/bin:$PATH"
+test -d "/opt/informix/bin" && PATH="/opt/informix/bin:$PATH"
+test -d "/opt/puppetlabs/bin" && PATH="/opt/puppetlabs/bin:$PATH"
+test -d "/shared/orbis/Support/CustomerDB" && PATH="/shared/orbis/Support/CustomerDB:$PATH"
+test -d "/shared/prj/openbet/appserv/rhel/6/x86_64/bin" && PATH="/shared/prj/openbet/appserv/rhel/6/x86_64/bin:$PATH"
+test -d "/shared/prj/openbet/liveserv/rhel/6/x86_64/sbin" && PATH="/shared/prj/openbet/liveserv/rhel/6/x86_64/sbin:$PATH"
+test -d "/usr/java/latest/bin" && PATH="/usr/java/latest/bin:$PATH"
+test -d "/usr/lib64/qt-3.3/bin" && PATH="/usr/lib64/qt-3.3/bin:$PATH"
 
 unset LS_COLORS
 unset LSCOLORS
