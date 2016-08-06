ab0157278ccd3ec02df6e03b4ed2752f4cd020a2
DictumMortuum <theio.vrefos@gmail.com>
2016-08-06 11:36:09 +0300

    couchdb cors

---
+++ .zsh/autoload/couchdb.zsh
@@ -0,0 +1,8 @@
+function @couchdb_cors() {
+  # $1 is in the form of http://adminname:password@localhost:5984
+  curl -X PUT $1/_config/httpd/enable_cors -d '"true"'
+  curl -X PUT $1/_config/cors/origins -d '"*"'
+  curl -X PUT $1/_config/cors/credentials -d '"true"'
+  curl -X PUT $1/_config/cors/methods -d '"GET, PUT, POST, HEAD, DELETE"'
+  curl -X PUT $1/_config/cors/headers -d '"accept, authorization, content-type, origin, referer, x-csrf-token"'
+}
