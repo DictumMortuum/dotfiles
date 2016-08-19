3b462a62ff65a90b1173fc0f8af377a16b59aec1
DictumMortuum <theio.vrefos@gmail.com>
2016-08-19 11:24:50 +0300

    [zsh] mkdir autoload

--- .zsh/autoload/mkdir.zsh
+++ .zsh/autoload/mkdir.zsh
@@ -5,5 +5,12 @@ function @mkcd() {
 
 # Create a directory like "year-month-day" (i.e. 2008-08-31)
 function @mkd() {
-  mkdir `date +%F` && cd `date +%F`
+  mkdir $(date +%F) && cd $(date +%F)
+}
+
+# Create a temp directory and change into it.
+function @mkt() {
+  tpl="temp.$(date +%F).XXXXXXXXXX"
+  temp=$(mktemp -d -p . $tpl)
+  cd $temp
 }
