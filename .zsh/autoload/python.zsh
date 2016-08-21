0ec9ee5dcc149a2a48c693424b126858c8af43e2
DictumMortuum <theio.vrefos@gmail.com>
2016-08-21 15:30:11 +0300

    [zsh] update @serve to work with python3

--- .zsh/autoload/python.zsh
+++ .zsh/autoload/python.zsh
@@ -1,12 +1,13 @@
+#
+# Usage: @serve [port]
+#
 function @serve() {
   version=$(python --version 2>&1)
-
-  #echo $version
-  #if [ "$version" == "*2.*" ]; then
+  echo "Using $version ..."
+  if [[ $version == *2.* ]]; then
     module="SimpleHTTPServer"
-  #elif [ "$version" == "3.*" ]; then
-  #  module="http.server"
-  #fi
-
+  elif [[ $version == 3.* ]]; then
+    module="http.server"
+  fi
   python -m $module $1
 }
