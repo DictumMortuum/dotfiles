36358e063077b057e345f6dffa742c84ad5f6384
DictumMortuum <theio.vrefos@gmail.com>
2016-08-31 19:22:41 +0300

    [install] added apm

--- install.sh
+++ install.sh
@@ -1,7 +1,26 @@
 preinstall() {
   sudo add-apt-repository --yes ppa:webupd8team/atom
   sudo apt-get update
-  sudo apt-get install --yes git zsh vim unity-tweak-tool tcl nodejs npm curl wget cvs ssh gawk atom
+  sudo apt-get install --yes git
+  sudo apt-get install --yes zsh
+  sudo apt-get install --yes vim
+  sudo apt-get install --yes unity-tweak-tool
+  sudo apt-get install --yes tcl
+  sudo apt-get install --yes nodejs
+  sudo apt-get install --yes npm
+  sudo apt-get install --yes curl
+  sudo apt-get install --yes wget
+  sudo apt-get install --yes cvs
+  sudo apt-get install --yes ssh
+  sudo apt-get install --yes gawk
+  sudo apt-get install --yes atom
+  apm install --no-confirm atom-material-ui
+  apm install --no-confirm base16-syntax
+  apm install --no-confirm file-icons
+  apm install --no-confirm language-tcl
+  apm install --no-confirm language-viml
+  apm install --no-confirm linter
+  apm install --no-confirm linter-eslint
 }
 
 install() {
