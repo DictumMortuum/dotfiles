d37cb20a117428b36309507de8721f14d008787e
DictumMortuum <theio.vrefos@gmail.com>
2017-02-23 14:34:31 +0200

    [zsh] DOT_HOME

--- .zsh/default.zsh
+++ .zsh/default.zsh
@@ -22,10 +22,33 @@ export HOSTFILE
 : ${LC_ALL:="en_US.UTF-8"}
 export LANG LANGUAGE LC_CTYPE LC_ALL
 
+# modules
+NOT_INCLUDED=()
+
+require() {
+  if type $1 > /dev/null 2>&1 ; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+include() {
+  if ! source $1; then
+    NOT_INCLUDED+=($(basename $1))
+  fi
+}
+
+foreach file in $(ls $SCRIPTS/* $THEMES/*); do
+  include $file
+done
+
 # PATH
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
 test -d "/snap/bin" && PATH="/snap/bin:$PATH"
 test -d "/usr/games" && PATH="/usr/games:$PATH"
 test -d "/usr/local/games" && PATH="/usr/local/games:$PATH"
 test -d "$HOME/.dotfiles/bin" && PATH="$HOME/.dotfiles/bin:$PATH"
-export PATH
+
+# condense PATH entries
+export PATH=$(path.tcl)
--- .zshrc
+++ .zshrc
@@ -1,26 +1,2 @@
-DOT_HOME=$HOME
-source $DOT_HOME/.zsh/default.zsh
-NOT_INCLUDED=()
-
-require() {
-  if type $1 > /dev/null 2>&1 ; then
-    return 1
-  else
-    return 0
-  fi
-}
-
-include() {
-  if ! source $1; then
-    NOT_INCLUDED+=($(basename $1))
-  fi
-}
-
-foreach file in $(ls $SCRIPTS/* $THEMES/*); do
-  include $file
-done
-
+DOT_HOME=$HOME && source $DOT_HOME/.zsh/default.zsh
 [[ $HOSTNAME != apparatus ]] && echo "Skipping: $NOT_INCLUDED"
-
-# condense PATH entries
-PATH=$(path.tcl)
