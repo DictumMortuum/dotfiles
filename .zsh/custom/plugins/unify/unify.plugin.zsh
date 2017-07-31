8d85d7f48e981e1773a3b9275fbd69690410d247
Δημήτρης Ραβιόλος <dictummortuum339@gmail.com>
2017-07-31 10:55:14 +0300

    [zsh] created a function that applies the proxy to git and npm; takes less time to execute since it is on demand, not on every zsh shell startup

--- .zsh/custom/plugins/unify/unify.plugin.zsh
+++ .zsh/custom/plugins/unify/unify.plugin.zsh
@@ -6,11 +6,8 @@ export https_proxy=http://172.28.12.5:8080/
 export no_proxy='localhost,127.0.0.0/8,::1,*.global-ad.net,*.global-intra.net,*.unify-ath.net,10.0.0.0/8'
 alias git_osv="/home/rd905075/.dotfiles/bin/git_osv.sh --gitlocal /home/rd905075/Code/production "
 
-if [[ ! $(dot s | grep .gitconfig) ]]; then
+function @unify_proxy() {
   git config --global http.proxy $http_proxy
-fi
-
-if [[ ! $(dot s | grep .npmrc) ]]; then
   npm config set proxy $http_proxy
   npm config set https-proxy $https_proxy
-fi
+}
