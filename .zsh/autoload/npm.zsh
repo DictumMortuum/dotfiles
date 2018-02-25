cb7f4c30f8f68a40a4c7b0abd4c6e214df93b421
DictumMortuum <theio.vrefos@gmail.com>
2018-02-25 19:54:43 +0200

    [git][npm] removed node_modules, removed hook

--- .zsh/autoload/npm.zsh
+++
@@ -1,7 +0,0 @@
-require npm && return 1
-
-# global npm configuration
-export NPM_CONFIG_PREFIX="$DOT_HOME/.dotfiles/node_modules"
-
-# update path
-PATH="$DOT_HOME/.dotfiles/node_modules/bin:$PATH"
