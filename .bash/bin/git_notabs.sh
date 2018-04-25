d9cd114a1e2f391644c493e20c4c6767d1546b6b
DictumMortuum <theio.vrefos@gmail.com>
2018-04-25 13:01:33 +0300

    [bash] -eq is for integers

--- .bash/bin/git_notabs.sh
+++ .bash/bin/git_notabs.sh
@@ -8,7 +8,7 @@
 #   notabs = true
 #
 
-if [[ $(git config hooks.notabs) -eq "true" ]]; then
+if [[ $(git config hooks.notabs) = "true" ]]; then
   if git diff --cached | grep -P '^\+\t' > /dev/null ; then
     echo "pre-commit: tabs detected in commit."
     exit 1
