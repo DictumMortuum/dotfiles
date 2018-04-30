9ca3ac3b914f16a5633778b36b0debe2d64b29d2
Δημήτρης Ραβιόλος <dictummortuum339@gmail.com>
2018-04-30 15:54:36 +0300

    [zsh] changed pure setup

--- .zsh/autoload/plugins.zsh
+++ .zsh/autoload/plugins.zsh
@@ -1,7 +1,12 @@
 # Plugins
 [[ $ZSH_VERSION == <5->* ]] && source $PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-source $PLUGINS/zsh-async/async.plugin.zsh
-source $PLUGINS/pure/pure.plugin.zsh
+#source $PLUGINS/zsh-async/async.plugin.zsh
+#source $PLUGINS/pure/pure.plugin.zsh
+
+fpath=( "$PLUGINS" $fpath )
+
+autoload -U promptinit; promptinit
+prompt pure
 
 BASE16_SHELL=$DOT_HOME/.dotfiles/base16-shell
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
