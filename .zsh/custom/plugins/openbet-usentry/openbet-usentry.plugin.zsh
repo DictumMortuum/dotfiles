218fda25d85fe1a46182b1ea582696c422b41fc5
DictumMortuum <theio.vrefos@gmail.com>
2018-04-30 15:38:18 +0300

    [openbet] added regression

--- .zsh/custom/plugins/openbet-usentry/openbet-usentry.plugin.zsh
+++ .zsh/custom/plugins/openbet-usentry/openbet-usentry.plugin.zsh
@@ -1,4 +1,6 @@
 @sync_cfg() {
   rsync -avz ~/Code/us-entry-cfg ops-cust:/opt/openbet/users/draviolo/
   rsync -avz ~/Code/us-entry-cfg ops-sports:/opt/openbet/users/draviolo/
+  rsync -avz ~/Code/us-entry-cfg regression-cust:/opt/openbet/users/draviolo/
+  rsync -avz ~/Code/us-entry-cfg regression-sports:/opt/openbet/users/draviolo/
 }
