33eef855ac7ccf470507e73b278be43d6561f281
Δημήτρης Ραβιόλος <dictummortuum339@gmail.com>
2018-05-03 13:00:52 +0300

    [tclsh][zsh] TCLLIBPATH and pure

--- .zsh/autoload/tclsh.zsh
+++ .zsh/autoload/tclsh.zsh
@@ -10,5 +10,5 @@ PATH="$DOT_HOME/.tclsh/bin:$PATH"
 # This path will be added to auto_path tcl global.
 # It will check that folder and its subfolders for pkgIndex.tcl files to source.
 # If a package is required.
-export TCLLIBPATH="$DOT_HOME:$TCLLIBPATH"
+export TCLLIBPATH="$DOT_HOME/.tclsh:$DOT_HOME/Code:$TCLLIBPATH"
 type tcllibpath.tcl >/dev/null 2>&1 && TCLLIBPATH=$(tcllibpath.tcl)
