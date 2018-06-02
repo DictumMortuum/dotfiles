6ca976151ed48073a9f79911b18f3dd9d3f27b14
DictumMortuum <theio.vrefos@gmail.com>
2018-06-02 11:39:24 +0300

    [zsh] shortcuts for US entry

--- .zsh/custom/plugins/openbet-usentry/openbet-usentry.plugin.zsh
+++ .zsh/custom/plugins/openbet-usentry/openbet-usentry.plugin.zsh
@@ -4,3 +4,15 @@
   rsync -avz ~/Code/us-entry-cfg regression-cust:/opt/openbet/users/draviolo/
   rsync -avz ~/Code/us-entry-cfg regression-sports:/opt/openbet/users/draviolo/
 }
+
+@logs() {
+  case $1 in
+    oxi*)
+      CMD="obcontrol --app=oxixmlserver_sports --tail"
+    ;;
+    xsys*)
+      CMD='tail -f /opt/openbet/logs/xsys_sync_queue-sports/xsys_sync_queue_reconcile-sports.log.$(date +%F_%H)'
+  esac
+
+  ssh sports2 "$CMD"
+}
