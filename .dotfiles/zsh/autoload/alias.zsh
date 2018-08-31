65c670b52fd59deb8651687325157aad06074000
Dimitris Raviolos <dictummortuum339@gmail.com>
2018-08-31 15:09:59 +0300

    [python] checklist initial

--- .dotfiles/zsh/autoload/alias.zsh
+++ .dotfiles/zsh/autoload/alias.zsh
@@ -3,7 +3,7 @@ require alias && return 1
 # https://news.ycombinator.com/item?id=11070797
 # http://stackoverflow.com/questions/1030169/easy-way-pull-latest-of-all-submodules
 alias dot="git --git-dir=$HOME/dotfiles.git --work-tree=$HOME"
-alias notes="git --git-dir=$HOME/notes/.git --work-tree=$HOME/notes"
+alias notes="git --git-dir=$HOME/Notes/.git --work-tree=$HOME/notes"
 
 # Assumes HIST_IGNORE_SPACE is set
 # making aliases to certain commands to start with space to be ignored from history
@@ -17,9 +17,15 @@ alias ψδ=" cd"
 alias λσ=" ls"
 alias γιτ="git"
 
-# 
+# time & date
 alias today="date +%Y-%m-%d"
 
 # Ubuntu custom shortcuts
 # ctrl + alt + d
 # sh -c 'xdotool type --clearmodifiers -- "$(date +%Y-%m-%d)"'
+
+# Using checklist: https://github.com/marinintim/checklist
+alias cl="checklist -f ~/Notes/checklist/checklist.txt"
+# daily list
+alias dl="checklist -f ~/Notes/checklist/checklist.$(date +%Y-%m-%d).txt"
+
