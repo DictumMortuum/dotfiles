375c77978bf0c6279a034af27257aaa6a0348914
Dimitris Raviolos <dictummortuum339@gmail.com>
2018-09-27 16:21:42 +0300

    [zsh] restored base16-shell

--- .dotfiles/zsh/autoload/plugins.zsh
+++ .dotfiles/zsh/autoload/plugins.zsh
@@ -5,5 +5,8 @@ fpath=( "$PLUGINS" $fpath )
 autoload -U promptinit; promptinit
 prompt pure
 
-BASE16_SHELL=$HOME/.dotfiles/base16-shell
-[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+BASE16_SHELL="$HOME/.dotfiles/base16-shell"
+[ -n "$PS1" ] && \
+  [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+    eval "$("$BASE16_SHELL/profile_helper.sh")"
+
