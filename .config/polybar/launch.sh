e96d6052f51f205c4eafcf9667f057faf4af415c
DictumMortuum <theio.vrefos@gmail.com>
2018-12-04 13:11:39 +0200

    [polybar] monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,9 +4,6 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-export MONITOR1=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | head -n 1)
-export MONITOR2=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 2p)
-
 polybar bar &
 polybar second &
 
