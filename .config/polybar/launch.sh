569907f30d409db7e9afa12d1f96a7f153272b68
DictumMortuum <theio.vrefos@gmail.com>
2018-12-06 13:10:05 +0200

    [i3] polybar update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,7 +4,12 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar bar &
-polybar second &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar &
+  done
+else
+  polybar --reload bar &
+fi
 
 echo "Bars launched..."
