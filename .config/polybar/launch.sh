abf2970be5968cb8939415ece77454859217263f
Dimitris Raviolos <dictummortuum339@gmail.com>
2018-12-26 22:30:47 +0200

    [polybar] monitors rework

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,11 +2,11 @@
 
 killall -q polybar
 
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    INTERFACE=$(ls /sys/class/net | grep en | tail -n 1) MONITOR=$m polybar --reload bar &
+    MONITOR=$m polybar --reload bar &
   done
 else
   polybar --reload bar &
