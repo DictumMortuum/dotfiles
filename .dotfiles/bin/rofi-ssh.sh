b70140fb6793f79230fabb55fb18e9f77e84c031
Dimitris Raviolos <dictummortuum339@gmail.com>
2019-02-20 17:48:26 +0200

    [ssh] cleanup and rofi-ssh.sh update

--- .dotfiles/bin/rofi-ssh.sh
+++ .dotfiles/bin/rofi-ssh.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-platform=$(cat ~/.ssh/hosts | rofi -dmenu -matching fuzzy -only-match -location 0 -p "Box" )
-query=$(ls ~/.ssh/env | grep -v config | rofi  -dmenu -matching fuzzy -location 0 -p "Env" )
-i3-sensible-terminal -x ssh ${platform}.internal.${query}.sportsbook.sgdigital.com
+box=$(cat ~/.ssh/hosts | rofi -dmenu -matching fuzzy -location 0 -p "Box")
+[[ -z $box ]] && exit 0
+env=$(ls ~/.ssh/env | grep -v config | rofi  -dmenu -matching fuzzy -location 0 -p "Env")
+i3-sensible-terminal -x ssh ${box}.internal.${env}.sportsbook.sgdigital.com
 exit 0
 
