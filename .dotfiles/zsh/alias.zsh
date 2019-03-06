47853e46063b91540d4ef48b85168d1877b4225e
DictumMortuum <theio.vrefos@gmail.com>
2019-03-06 22:40:35 +0200

    [ansible] moving provisioning to ansible

--- .dotfiles/zsh/alias.zsh
+++ .dotfiles/zsh/alias.zsh
@@ -16,6 +16,7 @@ alias vi='vim'
 alias tempfox='firefox --new-instance --profile $(mktemp -d)'
 alias unixtime='date +%s%3N'
 alias branch='echo ${LOGNAME}-$(unixtime)'
+alias system-update='ANSIBLE_CONFIG=~/.dotfiles/ansible/ansible.cfg ansible-playbook ~/.dotfiles/ansible/main.yml'
 
 # Assumes HIST_IGNORE_SPACE is set
 # making aliases to certain commands to start with space to be ignored from history
