51457cfda6b142486d122b1f29f79593329911f8
Dimitris Raviolos <dictummortuum339@gmail.com>
2019-03-18 16:42:29 +0200

    [ssh] copy host to clipboard on alt+1

--- .dotfiles/bin/ssh.sh
+++ .dotfiles/bin/ssh.sh
@@ -3,5 +3,13 @@
 box=$(cat ~/.ssh/hosts | rofi -dmenu -matching fuzzy -location 0 -p "Box")
 [[ -z $box ]] && exit 0
 env=$(ls ~/.ssh/env | grep -v config | rofi -dmenu -matching fuzzy -location 0 -p "Env")
-i3-sensible-terminal -x ssh ${box}.internal.${env}.sportsbook.sgdigital.com
+ret=$?
+
+if [[ $ret = 10 ]]; then
+  # alt + 1
+  echo ${box}.internal.${env}.sportsbook.sgdigital.com | xclip
+else
+  i3-sensible-terminal -x ssh ${box}.internal.${env}.sportsbook.sgdigital.com
+fi
+
 exit 0
