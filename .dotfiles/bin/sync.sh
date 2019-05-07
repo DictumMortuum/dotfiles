353003253f6e0e875223f67d9583999fef1cb9a6
DictumMortuum <theio.vrefos@gmail.com>
2019-05-07 23:40:33 +0300

    sync.sh to synchronize git repos

---
+++ .dotfiles/bin/sync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+GIT_DIR=$1
+GIT_BRANCH=${2:-master}
+USER=$(id -un)
+HOST=$(hostname)
+
+git -C $GIT_DIR pull origin ${GIT_BRANCH}
+git -C $GIT_DIR add -A
+git -C $GIT_DIR commit -m "${USER}@${HOST}" && git -C $GIT_DIR push origin ${GIT_BRANCH}
