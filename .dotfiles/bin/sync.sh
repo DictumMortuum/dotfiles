4e6d9be9a4c8931e6c489e09d1d2ea822f356fcd
Dimitris Raviolos <dictummortuum339@gmail.com>
2019-05-08 14:31:43 +0300

    sync.sh should push even if there is no commit

--- .dotfiles/bin/sync.sh
+++ .dotfiles/bin/sync.sh
@@ -7,4 +7,5 @@ HOST=$(hostname)
 
 git -C $GIT_DIR pull origin ${GIT_BRANCH}
 git -C $GIT_DIR add -A
-git -C $GIT_DIR commit -m "${USER}@${HOST}" && git -C $GIT_DIR push origin ${GIT_BRANCH}
+git -C $GIT_DIR commit -m "${USER}@${HOST}"
+git -C $GIT_DIR push origin ${GIT_BRANCH}
