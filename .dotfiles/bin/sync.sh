a6769f53f460e8f5df9268f175cc26b6c5137735
DictumMortuum <theio.vrefos@gmail.com>
2019-05-12 04:12:06 +0300

    sync.sh will send a notification to dunst via notify-send

--- .dotfiles/bin/sync.sh
+++ .dotfiles/bin/sync.sh
@@ -4,8 +4,9 @@ GIT_DIR=$1
 GIT_BRANCH=${2:-master}
 USER=$(id -un)
 HOST=$(hostname)
+THIS=$(basename $0)
 
 git -C $GIT_DIR pull origin ${GIT_BRANCH}
 git -C $GIT_DIR add -A
-git -C $GIT_DIR commit -m "${USER}@${HOST}"
+git -C $GIT_DIR commit -m "${USER}@${HOST}" && notify-send "${THIS}" "${GIT_DIR}"
 git -C $GIT_DIR push origin ${GIT_BRANCH}
