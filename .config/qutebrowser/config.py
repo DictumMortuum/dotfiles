d36368989e728fc80eb3480fd4e4ba9d2da9e515
Dimitris Raviolos <dictummortuum339@gmail.com>
2019-05-17 16:12:38 +0300

    [qutebrowser] added config for proxy

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,6 +1,7 @@
 config.bind('<Ctrl-Shift-y>', 'hint links spawn --detach mpv --force-window yes {hint-url}')
+config.bind('<Ctrl-Shift-p>', 'set content.proxy http://bop.ps.gameop.net:8080"')
+
 c.content.headers.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.128 Safari/537.36"
-#c.tabs.position = "bottom"
 c.tabs.padding = {"top": 10, "bottom": 10, "left": 10, "right": 10}
 c.fonts.monospace = 'Hack'
 c.fonts.tabs = '10pt monospace'
