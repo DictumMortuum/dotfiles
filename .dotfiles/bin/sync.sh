245eeed0c547c1c5415017a595b63c228ebd7e9e
DictumMortuum <theio.vrefos@gmail.com>
2019-06-16 16:31:53 +0300

    [cron] some env variables needed to be set to send dunst notifications from the cronjob

--- .dotfiles/bin/sync.sh
+++ .dotfiles/bin/sync.sh
@@ -5,6 +5,10 @@ GIT_BRANCH=${2:-master}
 USER=$(id -un)
 HOST=$(hostname)
 THIS=$(basename $0)
+ID=$(id -u)
+
+export XDG_RUNTIME_DIR=/run/user/$ID
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$ID/bus
 
 git -C $GIT_DIR pull origin ${GIT_BRANCH}
 git -C $GIT_DIR add -A
