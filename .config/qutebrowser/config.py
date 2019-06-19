017e191e35c99692b8be4af7b3d37805a1120704
Dimitris Raviolos <dictummortuum339@gmail.com>
2019-06-19 17:15:41 +0300

    [qutebrowser] the one on the apt-repositories is several versions old (1.1.1), so manually upgrading to 1.6.3

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,21 +3,37 @@ c.tabs.padding = {"top": 5, "bottom": 5, "left": 0, "right": 0}
 c.fonts.monospace = 'Hack'
 c.fonts.tabs = '10pt monospace'
 config.source('nord-qutebrowser.py')
-c.colors.tabs.bar.bg = "#2E3440"
-c.colors.tabs.even.bg = "#2E3440"
-c.colors.tabs.even.fg = "#ffffff"
-c.colors.tabs.odd.bg = "#2E3440"
-c.colors.tabs.odd.fg = "#ffffff"
-c.colors.tabs.selected.even.bg = "#4c566a"
-c.colors.tabs.selected.even.fg = "#ffffff"
-c.colors.tabs.selected.odd.bg = "#4c566a"
-c.colors.tabs.selected.odd.fg = "#ffffff"
+foreground = "#ffffff"
+background = "#2e3440"
+selected = "#4c566a"
+c.colors.tabs.bar.bg = background
+c.colors.tabs.even.bg = background
+c.colors.tabs.even.fg = foreground
+c.colors.tabs.odd.bg = background
+c.colors.tabs.odd.fg = foreground
+c.colors.tabs.selected.even.bg = selected
+c.colors.tabs.selected.even.fg = foreground
+c.colors.tabs.selected.odd.bg = selected
+c.colors.tabs.selected.odd.fg = foreground
 c.hints.chars = "asdfkl"
 c.hints.border = "1px solid #2e3440"
-c.colors.hints.bg = "#2e3440"
-c.colors.hints.fg = "#ffffff"
+c.colors.hints.bg = background
+c.colors.hints.fg = foreground
 c.colors.hints.match.fg = "#bf616a"
-c.url.searchengines = {"DEFAULT": "https://duckduckgo.com/?q={}", "s": "https://stash.int.openbet.com/plugins/servlet/search?q=\"{}\""}
+
+c.url.searchengines = {
+  "DEFAULT": "https://duckduckgo.com/?q={}",
+  "s": "https://stash.int.openbet.com/plugins/servlet/search?q=\"{}\"",
+  "j": "https://jira.openbet.com/issues/?jql=text~\"{}\""
+}
 
 config.bind('<Ctrl-Shift-y>', 'hint links spawn --detach mpv --force-window yes {hint-url}')
 config.bind('<Ctrl-Shift-p>', 'set content.proxy http://bop.ps.gameop.net:8080"')
+
+def content_persist(p):
+  with config.pattern(p) as p:
+    p.content.media_capture = True
+
+content_persist('*://meet.google.com/')
+content_persist('*://bluejeans.com/')
+
