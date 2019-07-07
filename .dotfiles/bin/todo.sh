35d0c1bc3308efd229f269086c7343b371309a10
DictumMortuum <theio.vrefos@gmail.com>
2019-07-07 23:05:00 +0300

    [todo] initial

---
+++ .dotfiles/bin/todo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TODO=/tmp/$(date +%F)
+touch $TODO
+
+case $1 in
+  add) cat $TODO | rofi -dmenu >> $TODO ;;
+  del) sed -i '1d' $TODO ;;
+  *) ;;
+esac
+
+pkill -SIGRTMIN+10 i3blocks
