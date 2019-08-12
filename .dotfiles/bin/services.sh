9a66d9782e732b74b914aba2499f4f50c996ffab
DictumMortuum <theio.vrefos@gmail.com>
2019-08-12 12:25:42 +0300

    [bin] now services.sh supports multi-select

--- .dotfiles/bin/services.sh
+++ .dotfiles/bin/services.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 SERVICES=${SERVICES_BASE}/services
-SELECTION=$(cat $SERVICES | rofi -dmenu)
+SELECTION=$(cat $SERVICES | rofi -dmenu -multi-select)
 [[ -z $SELECTION ]] && exit 0
-MODE=$(echo -e "restart\nstart\nstop" | rofi -dmenu)
+MODE=$(echo -e "restart\nstart\nstop\nenable" | rofi -dmenu)
 [[ -z $MODE ]] && exit 0
-echo "sudo systemctl $MODE $SELECTION" | xclip -sel c -f | xclip -sel p
+SERVICES=$(echo "$SELECTION" | tr '\n' ' ')
+echo "sudo systemctl $MODE $SERVICES" | xclip -sel c -f | xclip -sel p
 
