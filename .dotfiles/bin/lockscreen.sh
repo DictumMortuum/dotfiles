946a135b8265d10be4144bab55f4d1ce7b05efe3
DictumMortuum <theio.vrefos@gmail.com>
2019-10-10 16:57:46 +0300

    [i3lock] deprecated i3lock-color

--- .dotfiles/bin/lockscreen.sh
+++ .dotfiles/bin/lockscreen.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 
 TMPBG=/tmp/screen.png
-rectangles=" "
 
 revert() {
   xset dpms 0 0 0
 }
 
-SR=$(xrandr --query | grep ' connected' | grep -o '[0-9][0-9]*x[0-9][0-9]*[^ ]*')
-for RES in $SR; do
-  SRA=(${RES//[x+]/ })
-  CX=$((${SRA[2]} + 25))
-  CY=$((${SRA[1]} - 80))
-  rectangles+="rectangle $CX,$CY $((CX+300)),$((CY-80)) "
-done
-
-scrot $TMPBG && convert $TMPBG -scale 5% -scale 2000% -draw "fill black fill-opacity 0.4 $rectangles" $TMPBG
+scrot $TMPBG && convert $TMPBG -scale 5% -scale 2000% $TMPBG
 
 xkb-switch -s us
 trap revert SIGHUP SIGINT SIGTERM
 
-~/.local/bin/i3lock -i $TMPBG --timepos='x+120:h-120' --clock \
-  --insidecolor=00000000 --ringcolor=ffffffff --line-uses-inside \
-  --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
-  --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
-  --ringvercolor=ffffffff --ringwrongcolor=ffffffff --indpos="x+290:h-120" \
-  --radius=20 --ring-width=3 --veriftext="" --wrongtext="" \
-  --timecolor="ffffffff" --datecolor="ffffffff"
+i3lock -u -e -i $TMPBG
 
 rm $TMPBG
